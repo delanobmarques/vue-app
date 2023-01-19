e9024fce73174de1aedfd59aa4fed3fc0f953cb5
delanobmarques <delanobmarques@gmail.com>
2023-01-19 12:00:49 -0400

    outputting lists with v-for

--- app.js
+++ app.js
@@ -4,9 +4,17 @@ const app = Vue.createApp({
     data() {
         return {
             showBooks: false,
-            title: 'The Final Empire',
-            author: 'Brandon Sanderson',
-            age: 43,
+            // title: 'The Final Empire',
+            // author: 'Brandon Sanderson',
+            // age: 43,
+            books: [
+                { title: 'The Alchemist', author: 'Paulo Coelho'},
+                { title: 'The Da Vinci Code', author: 'Dan Brown'},
+                { title: 'Think and Grow Rich', author: 'Napolean Hill'},
+                { title: 'The Lord of the Rings', author: 'J.R.R. Tolkien'},
+                { title: "Harry Potter and the Philosopher's Stone", author: 'J.K. Rowling'},
+                { title: 'The Lion, the Witch, and the Wardrobe', author: 'J C.S. Lewis'},
+            ],
             x: 0,
             y: 0
         }
